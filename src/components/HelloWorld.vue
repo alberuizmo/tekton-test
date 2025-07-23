754b42107a5e4aa0ac6185dbc17c175780ada6f0
alberto ruiz <albertojruizm@gmail.com>
2025-07-23 07:35:01 -0500

    first test

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{ msg: string }>();
+</script>
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { resolve } from "path";
@@ -14,4 +15,12 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  test: {
+    globals: true,
+    environment: "happy-dom",
+    include: ["**/__tests__/**/*.{test,spec}.{ts,tsx}", "**/*.{test,spec}.{ts,tsx}"],
+    coverage: {
+      reporter: ["text", "html"],
+    },
+  },
 });
